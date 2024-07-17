8fc8b2e8a015a421a935df52a484c0c933dd341f
Martins, Evens <EVENS_MARTINS@homedepot.com>
2024-07-17 00:53:04 -0300

    new envornment

---
+++ src/environments/environment.homol.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: `https://criptolab-auth-service-dot-pandapay.rj.r.appspot.com`,
+  apiTokenUrl: `https://criptolab-token-api-dot-pandapay.rj.r.appspot.com`,
+  apiEventsUrl: `https://events.coins-external.com:8088`,
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: `https://criptolab-auth-service-dot-pandapay.rj.r.appspot.com`,
-  apiTokenUrl: `https://criptolab-token-api-dot-pandapay.rj.r.appspot.com`,
+  apiUrl: `https://criptolab-auth-service-prod-dot-pandapay.rj.r.appspot.com`,
+  apiTokenUrl: `https://criptolab-token-api-prod-dot-pandapay.rj.r.appspot.com`,
   apiEventsUrl: `https://events.coins-external.com:8088`,
 };
