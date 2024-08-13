e62130614232a73350b75b86c07daa596bdc2326
Martins, Evens <EVENS_MARTINS@homedepot.com>
2024-08-13 03:51:40 -0300

    create password streght validator

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Auth } from '../../../services/auth.service';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 
@@ -32,11 +32,7 @@ export class SignupComponent implements OnInit {
   alertTitle: string;
   alertMessage: string;
 
-  public formPreSignup: FormGroup = new FormGroup({
-    'email': new FormControl(null),
-    'password': new FormControl(null),
-    'confirmPassword': new FormControl(null)
-  })
+  public formPreSignup: FormGroup;
 
   public formSignup: FormGroup = new FormGroup({
     'name': new FormControl(null),
@@ -46,68 +42,154 @@ export class SignupComponent implements OnInit {
   public step1 = false;
   public step2 = false;
 
-  public errorMessage : string
+  public errorMessage: string
 
   loader: boolean = false
 
+  passwordStrength: string = '';
+  passwordRules = {
+    hasUpperCase: false,
+    hasLowerCase: false,
+    hasNumbers: false,
+    hasSpecialChars: false,
+    lengthValid: false,
+    confirmPassword: false,
+  };
+
   constructor(
     private auth: Auth,
-    private router: Router) { 
-    }
+    private router: Router,
+    private fb: FormBuilder) {
+    this.formPreSignup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator.bind(this)]],
+      confirmPassword: ['', [Validators.required]],
+    }, {
+      validator: this.confirmPasswordValidator.bind(this)
+    });
+  }
 
   ngOnInit(): void {
     this.changeStep(true, false)
   }
 
+  passwordValidator(control) {
+    const password = control.value;
+    if (!password) return null;
+
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const lengthValid = password.length >= 8;
+    const confirmPassword = false;
+
+    this.passwordRules = { hasUpperCase, hasLowerCase, hasNumbers, hasSpecialChars, lengthValid, confirmPassword };
+
+    if (hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChars && lengthValid) {
+      this.passwordStrength = 'strong';
+    } else if (lengthValid && (hasUpperCase || hasLowerCase) && (hasNumbers || hasSpecialChars)) {
+      this.passwordStrength = 'medium';
+    } else {
+      this.passwordStrength = 'weak';
+    }
+
+    if (!this.passwordStrength) {
+      return { passwordInvalid: true };
+    }
+
+    if (password !== confirmPassword) {
+      this.passwordRules.confirmPassword = false;
+      return { passwordInvalid: true };
+    }
+
+    this.passwordRules.confirmPassword = true;
+
+    return null;
+  }
+
+  confirmPasswordValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password').value;
+    const confirmPassword = formGroup.get('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+      this.passwordRules.confirmPassword = false;
+      return { passwordInvalid: true };
+    }
+
+    this.passwordRules.confirmPassword = true;
+    return null
+  }
+
   public preSignup(): void {
     this.showLoader(true);
 
-    if(!this.formPreSignup.value.email 
-      || !this.formPreSignup.value.password 
-      || !this.formPreSignup.value.confirmPassword){
+    if (!this.formPreSignup.value.email
+      || !this.formPreSignup.value.password
+      || !this.formPreSignup.value.confirmPassword) {
       this.showAlert(false, "Informe todos os dados", "Não esqueça nenhum dado solicitado.");
       return
     }
 
-    if(this.formPreSignup.value.password != this.formPreSignup.value.confirmPassword){
+    const isValidEmail = (email: string) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
+
+    if (!isValidEmail(this.formPreSignup.value.email)) {
+      this.showAlert(false, "Erro na Senha", "Você deve informar um email válido");
+      return
+    }
+
+    if (this.formPreSignup.value.password != this.formPreSignup.value.confirmPassword) {
       this.showAlert(false, "Erro na Senha", "As senhas não conferem. Elas devem ser iguais!");
       return
     }
 
-    this.auth.preSignup(this.formPreSignup.value.email, this.formPreSignup.value.password)
-      .subscribe(data => {
-        const response: any = data;
-        if(response.user){
-          this.logged(response)
-          return
-        }
 
-        this.changeStep(false, true);
-      },
-      error => {
-        console.log(error)
-        this.showLoader(false);
-        this.showAlert(false, "Dados Incorretos", error);
-      })
+    const areAllRulesValid = () => {
+      return Object.values(this.passwordRules).every(rule => rule === true);
+    }
+
+    if (!areAllRulesValid()) {
+      this.showAlert(false, "Erro na Senha", "A senha deve estar de acordo com todas as regras");
+      return
+    }
+
+    // this.auth.preSignup(this.formPreSignup.value.email, this.formPreSignup.value.password)
+    //   .subscribe(data => {
+    //     const response: any = data;
+    //     if (response.user) {
+    //       this.logged(response)
+    //       return
+    //     }
+
+    //     this.changeStep(false, true);
+    //   },
+    //     error => {
+    //       console.log(error)
+    //       this.showLoader(false);
+    //       this.showAlert(false, "Dados Incorretos", error);
+    //     })
   }
 
-  public signup(){
+  public signup() {
     this.showLoader(true);
     this.auth.signup(this.formPreSignup.value.email, this.formPreSignup.value.password, this.formSignup.value.name, this.formSignup.value.code)
       .subscribe(data => {
         const response: any = data;
-        if(response.user){
+        if (response.user) {
           this.logged(response)
         }
       },
-      error => {
-        console.log(error)
-        this.showLoader(false);
-        this.showAlert(false, "Dados Incorretos", error);
-      })
+        error => {
+          console.log(error)
+          this.showLoader(false);
+          this.showAlert(false, "Dados Incorretos", error);
+        })
   }
 
-  resendCode(){
+  resendCode() {
     this.showLoader(true);
     this.auth.preSignup(this.formPreSignup.value.email, this.formPreSignup.value.password)
       .subscribe(data => {
@@ -115,21 +197,21 @@ export class SignupComponent implements OnInit {
         this.changeStep(false, true);
         this.showLoader(false);
       },
-      error => {
-        console.log(error)
-        this.showLoader(false);
-        this.showAlert(false, "Dados Incorretos", error);
-      })
+        error => {
+          console.log(error)
+          this.showLoader(false);
+          this.showAlert(false, "Dados Incorretos", error);
+        })
   }
 
-  logged(response: any){
+  logged(response: any) {
     localStorage.setItem('auth_token', response.authToken);
     localStorage.setItem('user_data', JSON.stringify(response.user));
     this.showLoader(false);
     this.router.navigate(['/my-tokens']);
   }
 
-  changeStep(step1: boolean, step2: boolean){
+  changeStep(step1: boolean, step2: boolean) {
     this.showLoader(false);
     this.step1 = step1;
     this.step2 = step2;
@@ -139,11 +221,11 @@ export class SignupComponent implements OnInit {
     this.loader = status
   }
 
-  showTerms(){
+  showTerms() {
     this.isTermsShowing = !this.isTermsShowing;
   }
 
-  showAlert(success: boolean, title: string, message: string){
+  showAlert(success: boolean, title: string, message: string) {
     this.showLoader(false);
     this.alertIcon = success ? "success" : "warning";
     this.alertTitle = title;
